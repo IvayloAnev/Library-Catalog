9fa65a10bdf1400efdfa10510cee07910c7033fb
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-11-15 17:18:16 +0200

    'Verify "Add Books" link is visible after login

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -36,6 +36,17 @@ test('Verify "My Books" link is visible after user login', async({page}) => {
     expect(isMyBooksLinkVisible).toBe(true);
 });
 
+test('Verify "Add Books" link is visible after user login', async({page}) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]','peter@abv.bg');
+    await page.fill('input[name="password"]','123456');
+    await page.click('input[type="submit"]');
+    const AddBooksLink = await page.$('a[href="/create"]');
+    const isAddBooksLinkVisible = await AddBooksLink.isVisible();
+    expect(isAddBooksLinkVisible).toBe(true);
+});
+
+
 
 
 
