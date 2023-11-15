7960f29723cda3ebd09791c7493df586f096796d
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-11-15 17:11:19 +0200

    Verify "My Books" link is visible after user login

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -26,6 +26,21 @@ test('Verify "All Books" link is visible after user login', async({page}) => {
     expect(isAllBooksLinkVisible).toBe(true);
 });
 
+test('Verify "My Books" link is visible after user login', async({page}) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]','peter@abv.bg');
+    await page.fill('input[name="password"]','123456');
+    await page.click('input[type="submit"]');
+    const MyBooksLink = await page.$('a[href="/profile"]');
+    const isMyBooksLinkVisible = await MyBooksLink.isVisible();
+    expect(isMyBooksLinkVisible).toBe(true);
+});
+
+
+
+
+
+
 test('Verify "Register" button is visible', async({page}) => {
     await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
